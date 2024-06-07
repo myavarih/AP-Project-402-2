12d48a2ad04914030b9ceac7d4631f8ed49fd2f8
myavarih <78411409+myavarih@users.noreply.github.com>
2024-06-07 19:46:10 +0330

    Bug Fix!

--- AP Project/MainWindow.xaml.cs
+++ AP Project/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace AP_Project
         public MainWindow()
         {
             InitializeComponent();
-
         }
+
     }
 }
--- AP Project/obj/Debug/net8.0-windows/AP Project.AssemblyInfo.cs
+++ AP Project/obj/Debug/net8.0-windows/AP Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AP Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88fe03894290430c27ef22f9bba91f1f6e6273d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9645a02953f8c8c98a35974c76f4fd33fb76500c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AP Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AP Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
