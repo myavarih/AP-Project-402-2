131907d95ee0596c0592d8b30624274fe3d0c4ce
myavarih <78411409+myavarih@users.noreply.github.com>
2024-06-26 07:48:46 +0330

    Skeletonn of log in and sign up pages

--- AP Project/SignUp.xaml.cs
+++ AP Project/SignUp.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Reflection.Emit;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -7,21 +10,23 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace AP_Project
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for SignUp.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SignUp : Window
     {
-        private void UserSignUpBtnClick(object sender, RoutedEventArgs e)
+        public SignUp()
         {
-           SignUp signUp = new SignUp();
-            signUp.Show();
-            this.Close();
+            InitializeComponent();
+        }
+
+        private void SignUpButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
--- AP Project/obj/Debug/net8.0-windows/AP Project.AssemblyInfo.cs
+++ AP Project/obj/Debug/net8.0-windows/AP Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AP Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9645a02953f8c8c98a35974c76f4fd33fb76500c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12d48a2ad04914030b9ceac7d4631f8ed49fd2f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("AP Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AP Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
